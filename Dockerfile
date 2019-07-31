13b5a45b5a0c0298e6f316c1cd2236ec3f3c0562
Lord Nighton <lordnightonnik@gmail.com>
2019-07-31 16:33:48 +0300

    Making the PoC usable by AWS BeansTalk

--- Dockerfile
+++ Dockerfile
@@ -8,3 +8,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
 
 CMD [ "python", "./dropbox.py" ]
+EXPOSE 8080
--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
-docker build -t dropbox -f ./dropbox.Dockerfile .
+docker build -t dropbox -f ./Dockerfile .
 docker run -it -p 8080:8080 --rm --name dropbox dropbox
 
 # Jump into an sh of a running dropbox container -- $ docker exec -it dropbox /bin/sh
+
+# docker tag d7d3413f68bc lordnighton/dropbox:latest
+# docker push lordnighton/dropbox
+# docker pull lordnighton/dropbox
+# docker login --username=lordnighton // then put the password
+# docker run -it -p 8080:8080 --rm --name dropbox lordnighton/dropbox
+# docker save lordnighton/dropbox:latest > dropbox.tar
+# zip dropbox.zip Dockerfile dropbox.py fileUploadForm.py requirements.txt templates/*
