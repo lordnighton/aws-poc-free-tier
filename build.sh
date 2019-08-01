0a2ab6c62ed44a5278fb0d89bd792a6f27714e65
Lord Nighton <lordnightonnik@gmail.com>
2019-08-01 12:24:22 +0300

    Boto-3 example provided for S3 buckets management

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ docker run -it -p 8080:8080 --rm --name dropbox dropbox
 
 # Jump into an sh of a running dropbox container -- $ docker exec -it dropbox /bin/sh
 
+# Docker image related stuff
+
 # docker tag d7d3413f68bc lordnighton/dropbox:latest
 # docker push lordnighton/dropbox
 # docker pull lordnighton/dropbox
@@ -10,3 +12,6 @@ docker run -it -p 8080:8080 --rm --name dropbox dropbox
 # docker run -it -p 8080:8080 --rm --name dropbox lordnighton/dropbox
 # docker save lordnighton/dropbox:latest > dropbox.tar
 # zip dropbox.zip Dockerfile dropbox.py fileUploadForm.py requirements.txt templates/*
+
+# AWS CLI related stuff
+# export PATH=/Users/lordnighton/Library/Python/3.7/bin:$PATH
